67d475419293bc8465e356e4f3db4812af24646c
royalfig <ryan.feigenbaum@gmail.com>
2020-01-28 15:42:58 -0500

    turned off logger

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(basicAuth({
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(
   bodyParser.urlencoded({
